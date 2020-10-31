733491605529d9fe8809309fe600df4a3fd3e852
Emmanuel <rivera128@live.com>
2020-10-31 13:45:56 -0500

    commit tree 842cae1d0e675660552bdcd3c853938fce5e2a2e

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    value_array = []
+    name_hash.collect do |key, value|
+        value_array << value
+    end
+    minimum = value_array[0]
+    for num in value_array
+        if num < minimum
+            minimum = num
+        end
+    end
+    name_hash.key(minimum)
 end
